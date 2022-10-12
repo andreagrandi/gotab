757dce67dab4ee2b93884bae34b207f2ebb2fa88
Andrea Grandi <a.grandi@gmail.com>
2022-10-12 08:31:57 +0200

    Move structs to the right place

--- cmd/tab/main.go
+++ cmd/tab/main.go
@@ -1,10 +1,1 @@
 package main
-
-type Item struct{}
-
-type TabInterface interface {
-	AddItem(i *Item) error
-	IsFull() bool
-	Settle() error
-	Total() int
-}
--- pkg/tab/tab.go
+++ pkg/tab/tab.go
@@ -1,1 +1,10 @@
 package tab
+
+type Item struct{}
+
+type TabInterface interface {
+	AddItem(i *Item) error
+	IsFull() bool
+	Settle() error
+	Total() int
+}
